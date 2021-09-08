b361db7462cfd59530f82924b4167b527921b6c5
lisethav <lisethav@gmail.com>
2021-09-07 23:58:32 -0500

    8-uppercase.py

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uppercase(str):
+    s = ''
+    for i in str:
+        if ord(i) in range(97, 123):
+            s += chr(ord(i) - 32)
+        else:
+            s += i
+    print("{}".format(s))
+    
