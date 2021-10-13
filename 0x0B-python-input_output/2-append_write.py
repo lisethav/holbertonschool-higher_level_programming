c34119bf34e88c5498ccf1265e6e325ab3505eae
lisethav <lisethav@gmail.com>
2021-10-12 22:08:32 -0500

    modify

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -7,5 +7,5 @@ and returns the number of characters added
 
 def append_write(filename="", text=""):
     """ append_write appends a text to a file """
-    with open(filename, mode="w", encoding='utf-8') as file:
+    with open(filename, mode="a", encoding='utf-8') as file:
         return(file.write(text))
