be4b8f16574f4b2cde0684e336f8d57d33f4f0fe
lisethav <lisethav@gmail.com>
2021-10-12 23:59:37 -0500

    pep8

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-a script that adds all arguments to a python
-list, and then save into a file
+Script that adds all arguments to a Python list, and then save them
+to a file
 """
 import sys
 import os
