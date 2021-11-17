496939fd5c5b5a232882cdde6649986af699e3da
lisethav <85521450+lisethav@users.noreply.github.com>
2021-11-17 00:57:28 -0500

    Update 8-count_89.sql

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,2 +1,2 @@
 -- displays the number of records with id = 89 in the table first_table of the database hbtn_0c_0
- SELECT count(98) FROM first_table WHERE id = 98
+ SELECT count(id) FROM first_table WHERE id = 98
