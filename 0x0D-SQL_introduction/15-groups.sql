cec4d67b7925a243154c6e85f36777b8ef15e5da
lisethav <lisethav@gmail.com>
2021-11-19 00:17:50 -0500

    15 fix descent

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- lists the number of records with the same score in the table second_table
-SELECT score, count(score) AS NUMBER FROM second_table GROUP BY score;
+SELECT score, count(score) AS NUMBER FROM second_table GROUP BY score ORDER BY score DESC;
