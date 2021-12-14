614616d0c4109ca7283b4d8fa502e1aca31897da
lisethav <lisethav@gmail.com>
2021-12-13 19:18:13 -0500

    commit two

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+// script that prints 3 lines
+const myVar = 'C is fun';
+const myVar2 = 'Python is cool';
+const myVar3 = 'JavaScript is amazing';
+console.log(myVar + "\n" + myVar2 + "\n" + myVar3);
