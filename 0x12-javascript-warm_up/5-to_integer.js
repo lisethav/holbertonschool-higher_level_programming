3bc911a94bcd6916a5448b162cbd4c16801f1008
lisethav <lisethav@gmail.com>
2021-12-13 21:29:34 -0500

    6-multi_languages_loop.js

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// script that prints My number: <first argument converted in integer> 
+// if the first argument can be converted to an integer
+const num = parseInt(process.argv[2])
+if (isNaN(num)) {
+    console.log('Not a number');
+}
+else {
+    console.log('My number:', num)
+}
