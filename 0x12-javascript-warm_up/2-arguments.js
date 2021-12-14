506662abe20168e0118f3a6068f856d8a0cf1488
lisethav <lisethav@gmail.com>
2021-12-13 19:40:39 -0500

    requerimientos

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 // script that prints a message depending of
 // the number of arguments passed
-if (process.argv.length <= 2){
+if (process.argv.length <= 2) {
     console.log('No argument');
 }
-else if (process.argv.length == 3){
+else if (process.argv.length == 3) {
     console.log('Argument found');
 }
-else{
+else {
     console.log('Arguments found');
 }
