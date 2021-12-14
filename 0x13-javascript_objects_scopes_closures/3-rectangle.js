1a9283f7521094852d0162808708fee4900d8e09
lisethav <lisethav@gmail.com>
2021-12-14 17:19:34 -0500

    fix

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,10 +1,16 @@
 #!/usr/bin/node
-//  class Rectangle that defines a rectangle
+// class Rectangle that defines a rectangle
 class Rectangle {
   constructor (w, h) {
     if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
     }
   }
 }
