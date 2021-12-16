5c3d7debcb412c16397d598146593944ce0be543
lisethav <lisethav@gmail.com>
2021-12-15 23:54:57 -0500

    fix

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,10 +1,18 @@
 #!/usr/bin/node
 // script that searches the second biggest integer in the list of arguments
-const numb = process.argv.slice(2);
-if (numb.length < 2) {
+const numb = process.argv;
+if (numb.length === 2 || numb.length === 3) {
   console.log(0);
 } else {
-  numb.sort((a, b) => a - b);
-  numb.pop();
-  console.log(numb.pop());
+  const array = [];
+  for (let a = 0; a < numb.lenght; a++) {
+    if (!isNaN(parseInt(numb[a]))) {
+      array.push(parseInt(numb[a]));
+    }
+  }
+  const large = Math.max(...array);
+  const init = array.indexOf(large);
+  array.splice(init, 1);
+  const second = Math.max(...array);
+  console.log(second);
 }
