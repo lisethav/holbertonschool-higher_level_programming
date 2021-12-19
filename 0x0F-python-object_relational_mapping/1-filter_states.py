0c67934968c88f84f3e86f1e61421e66770fa122
lisethav <lisethav@gmail.com>
2021-12-19 14:37:49 -0500

    error two

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -8,7 +8,7 @@ import sys
 if __name__ == "__main__":
 
     datab = MySQLdb.connect(host="localhost",  port=3306, user=sys.argv[1],
-                            passwd=sys.argv[2], datab=sys.argv[3])
+                            passwd=sys.argv[2], db=sys.argv[3])
 
     cursor = datab.cursor()
     cursor.execute('SELECT * FROM states WHERE name LIKE BINARY "N% ORDER BY id')
